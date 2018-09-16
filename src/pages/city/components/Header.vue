ca79766d7b98afef60ebb2d0304a13e5348eb313
Cosmogenik <394541411@qq.com>
2018-09-16 21:15:56 +0800

    add city-header

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class='iconfont city-back'>&#xe624;</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@styles/varibles.styl'
+  .header
+    position relative
+    height $headerHeight
+    line-height $headerHeight
+    overflow hidden
+    text-align center
+    font-size .32rem
+    color #fff
+    background $bgColor
+    .city-back
+      position absolute
+      top 0
+      left 0
+      font-size .4rem
+      width .64rem
+      color #fff
+</style>
