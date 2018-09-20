719aa8f4ddd7bd38e40bb72927d657daff1ff772
Cosmogenik <394541411@qq.com>
2018-09-20 11:24:51 +0800

    add animation

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -7,12 +7,15 @@
         <div class="banner-number"><span class="iconfont banner-icon">&#xe653;</span>{{gallaryImgs.length}}</div>
       </div>
     </div>
-    <common-gallary class="gallary" :imgs="gallaryImgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+    <fade-animation>
+      <common-gallary class="gallary" :imgs="gallaryImgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+    </fade-animation>
   </div>
 </template>
 
 <script>
 import CommonGallary from '@common/gallary/Gallary'
+import FadeAnimation from '@common/fade/FadeAnimation'
 export default {
   name: 'DetailBanner',
   data () {
@@ -27,7 +30,8 @@ export default {
     gallaryImgs: Array
   },
   components: {
-    CommonGallary
+    CommonGallary,
+    FadeAnimation
   },
   methods: {
     handleClickBanner () {
